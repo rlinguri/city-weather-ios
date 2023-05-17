595b9900d4cbfed5cd38120e3987866dd0aa606e
Roderic Linguri <linguri@digices.com>
2023-05-17 10:27:57 -0600

    0.1.1 | Task | Project setup (#1)
    * Update repository details
    
    * Create empty Xcode project
    
    * Add PR template and CHANGELOG

---
+++ Sources/Compile/ViewController.swift
@@ -0,0 +1,22 @@
+//
+// File:      ViewController.swift
+// Package:   CityWeather
+//
+// Author:    Roderic Linguri <938388@cognizant.com>
+// Copyright: © 2023 Roderic Linguri • All Rights Reserved
+// License:   MIT
+//
+// Version:   0.1.1
+// Requires:  iOS 15.6
+//            Swift 5.0
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
